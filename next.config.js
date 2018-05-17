37c8de889359772198ce3b6bd46df62cd2d3b667
Joe Brady <joe.brady@uk.ibm.com>
2018-05-17 10:39:02 +0100

    Add ESLint to webpack for yarn run dev

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS();
+
+module.exports = withCSS({
+  webpack(config, { dev }) {
+    if (dev) {
+      config.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "eslint-loader"
+      });
+    }
+    return config;
+  }
+});
