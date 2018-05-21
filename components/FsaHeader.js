9b6cb4ce568e6088155568d6cfd7d8fe050a3633
Joe Brady <joe.brady@uk.ibm.com>
2018-05-21 14:22:07 +0100

    Add landing page draft

--- components/FsaHeader.js
+++ components/FsaHeader.js
@@ -1,7 +1,19 @@
-const FsaHeader = () => (
-  <div>
-    <h1>Header</h1>
-  </div>
+import { TopNav, asAnchor } from "govuk-react";
+import CrownIcon from "@govuk-react/icon-crown";
+
+const AnchorTag = asAnchor("a");
+const link = "https://gov.uk";
+
+const Company = (
+  <AnchorTag href={link} target="new">
+    <TopNav.IconTitle icon={<CrownIcon width="36" height="32" />}>
+      GOV.UK
+    </TopNav.IconTitle>
+  </AnchorTag>
 );
+
+// TODO: links in the TopNav component need to be white rather than purple when visited.
+// TODO: decide whether to add the service name "Register a food business" here as per the prototype.
+const FsaHeader = () => <TopNav company={Company} />;
 
 export default FsaHeader;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import { hydrate, injectGlobal } from "react-emotion";
 import "normalize.css/normalize.css";
 import FsaLayout from "../components/FsaLayout";
-// import { Button } from "govuk-react";
+import { Header, Button } from "govuk-react";
+import { ButtonArrow } from "@govuk-react/icons";
+// TODO: remove dependency on @storybook/addon-knobs, remove from Package.json
+// import { boolean } from "@storybook/addon-knobs/react";
+import { P } from "../elements/P.element";
 
 // Adds server generated styles to emotion cache.
 // '__NEXT_DATA__.ids' is set in '_document.js'
@@ -12,13 +16,46 @@ if (typeof window !== "undefined" && typeof __NEXT_DATA__ !== "undefined") {
 
 injectGlobal`
   html, body {
-    font-family: sans-serif;
+    font-family: nta, 'Libre Franklin', sans-serif;
+    font-size: 19px;
+    color: #0b0c0c;
+  }
+
+  .bold {
+    font-weight: bold;
   }
 `;
 
 const Index = () => (
   <FsaLayout>
-    <div>Index</div>
+    <Header level={1}>Register a food business</Header>
+    {/* TODO: all paragraphs in this section need to be replaced with govuk-react components when ready */}
+    <P>
+      All food businesses which regularly produce or serve food to the public
+      need to be registered with their Local Authority. This service will send
+      your registration to the correct local authority based on your trading
+      location. Food businesses should only register{" "}
+      <span className="bold">28 days</span> before they begin trading. If you
+      are not sure when this food business will open, it may be too early to be
+      registering this business.
+    </P>
+    {/* TODO: the below paragraph needs to be the 'inset'/'panel' component. Govuk-react does not provide this yet. */}
+    <P>
+      During this registration, you may come across a few specialist terms,
+      which we have described below:
+    </P>
+    <Header level={3}>Food business operator</Header>
+    <P>
+      The operator is the person, charity or company who makes the decisions
+      about the food business, what it serves and how it operates.
+    </P>
+    <Header level={3}>Establishment</Header>
+    <P>
+      The establishment is the location or site of your food business. If it is
+      a mobile food business, please use the location where it is normally
+      stored overnight.
+    </P>
+    <Button icon={<ButtonArrow />}>Begin registration</Button>
   </FsaLayout>
 );
 
