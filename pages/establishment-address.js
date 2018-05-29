18819b3f5ed7b337e86a5c66d35b5d15782e43ff
A. P. Malde <anisha.malde@ibm.com>
2018-05-29 12:35:15 +0100

    Addition of validator package

--- pages/establishment-address.js
+++ pages/establishment-address.js
@@ -1,6 +1,13 @@
 import React from "react";
 import FsaLayout from "../components/FsaLayout";
+import { Header, InputField } from "govuk-react";
 
-const EstablishmentAddress = () => <FsaLayout />;
+const EstablishmentAddress = () => (
+  <FsaLayout>
+    <Header level={2}>What is the operators address?</Header>
+    <InputField hint={"First line of address"} />
+    <InputField hint={"Postcode"} />
+  </FsaLayout>
+);
 
 export default EstablishmentAddress;
