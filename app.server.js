1c09dd9d660f3683c8651ea6b9f2ce63f1a43152
Joe Brady <joe.brady@uk.ibm.com>
2018-05-29 14:44:47 +0100

    SDB-94 Create temporary session storage to store route JSON and user data

--- app.server.js
+++ app.server.js
@@ -11,6 +11,40 @@ const port = process.env.PORT || 3000;
 const startServer = async () => {
   const app = express();
 
+  ////////////////////////////////////////////////////////////
+  // WARNING: this session implementation is not production-ready.
+  // It uses in-memory session storage and a plaintext secret.
+  // It must be replaced before any production use.
+  // Upon replacement, run `yarn remove body-parser express-session`
+  // Ensure that all tests still pass after this step.
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.json());
+
+  const session = require("express-session");
+  app.use(
+    "/session",
+    session({
+      secret: "TEMPORARYSECRET",
+      resave: false,
+      saveUninitialized: true
+    })
+  );
+
+  app.get("/session", (req, res) => {
+    if (req.session.data) {
+      res.send(JSON.stringify(req.session.data));
+    } else {
+      res.send(null);
+    }
+  });
+
+  app.post("/session", (req, res) => {
+    req.session.data = JSON.stringify(req.body);
+    res.send(JSON.stringify("Data written to session"));
+  });
+  // END WARNING
+  ////////////////////////////////////////////////////////////
+
   await Next.prepare();
 
   app.get("*", (req, res) => {
