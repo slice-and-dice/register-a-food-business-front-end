2d782cb6cfae94ef5e34b85bf57ea8ecbcb1ba75
A. P. Malde <anisha.malde@ibm.com>
2018-05-29 16:25:44 +0100

    SDB-86 Create Application Complete page

---
+++ pages/application-complete.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FsaLayout from "../components/FsaLayout";
+import { Header } from "govuk-react";
+
+const ApplicationComplete = () => (
+  <FsaLayout>
+    <Header level={2}>Application Complete</Header>
+  </FsaLayout>
+);
+
+export default ApplicationComplete;
