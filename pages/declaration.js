19257a5e72fcf3cb7afce0b9c68b102d65ca0ea0
A. P. Malde <anisha.malde@ibm.com>
2018-05-29 12:55:43 +0100

    Creation of the Declaration Page of Submit Registration

---
+++ pages/declaration.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FsaLayout from "../components/FsaLayout";
+import { Header } from "govuk-react";
+
+const Declaration = () => (
+  <FsaLayout>
+    <Header level={2}>Declaration</Header>
+  </FsaLayout>
+);
+
+export default Declaration;
