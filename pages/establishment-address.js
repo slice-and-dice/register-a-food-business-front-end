88ac4a8eff139aa7c4523802de33aa5de042347c
A. P. Malde <anisha.malde@ibm.com>
2018-06-04 17:13:47 +0100

    Error messages and styling

--- pages/establishment-address.js
+++ pages/establishment-address.js
@@ -1,44 +1,61 @@
 import React from "react";
 import FsaLayout from "../components/FsaLayout";
-import { Header, Input, Button, Label } from "govuk-react";
+import { Header, Button, InputField } from "govuk-react";
 import SessionWrapper from "../components/SessionWrapper";
 import SessionInput from "../components/SessionInput";
 
 const EstablishmentAddress = props => (
   <FsaLayout>
     <Header level={2}>Establishment address</Header>
-
     <form action="/continue/establishment-address" method="post">
       <SessionInput {...props} />
-      <Label>First line of address</Label>
-      <Input
-        name="establishment_first_line"
+      <br />
+      <InputField
+        input={{ name: "establishment_first_line" }}
         id="establishment_first_line"
         // TODO APM: Decide on and implement validation for first line of address
         //Work out why validator errors fails in test
-        error={props.validatorErrors["establishment_first_line"]}
-      />
-      <Label>Street</Label>
-      <Input
-        type="text"
+        meta={{
+          touched: true,
+          error: props.validatorErrors["establishment_first_line"]
+        }}
+      >
+        First Line of address
+      </InputField>
+      <br />
+      <InputField
+        input={{ name: "establishment_street" }}
         id="establishment_street"
-        name="establishment_street"
-        error={props.validatorErrors["establishment_street"]}
-      />
-      <Label>Town or city</Label>
-      <Input
-        type="text"
+        meta={{
+          touched: true,
+          error: props.validatorErrors["establishment_street"]
+        }}
+      >
+        Street
+      </InputField>
+      <br />
+      <InputField
+        input={{ name: "establishment_town" }}
         id="establishment_town"
-        name="establishment_town"
-        error={props.validatorErrors["establishment_town"]}
-      />
-      <Label>Postcode</Label>
-      <Input
-        type="text"
+        meta={{
+          touched: true,
+          error: props.validatorErrors["establishment_town"]
+        }}
+      >
+        Town or city
+      </InputField>
+      <br />
+      <InputField
+        input={{ name: "establishment_postcode" }}
         id="establishment_postcode"
-        name="establishment_postcode"
-        error={props.validatorErrors["establishment_postcode"]}
-      />
+        meta={{
+          touched: true,
+          error: props.validatorErrors["establishment_postcode"]
+        }}
+      >
+        Postcode
+      </InputField>
+      <br />
       <Button type="submit">Continue</Button>
       <p>
         If you are registering a mobile food business, please use the location
