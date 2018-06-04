48c60f747c642f8af30aac7bf828b707f0b6804b
A. P. Malde <anisha.malde@ibm.com>
2018-06-04 14:37:37 +0100

    SDB-71 applying validation to inout fields

--- pages/establishment-address.js
+++ pages/establishment-address.js
@@ -1,13 +1,51 @@
 import React from "react";
 import FsaLayout from "../components/FsaLayout";
-import { Header, InputField } from "govuk-react";
+import { Header, Input, Button, Label } from "govuk-react";
+import SessionWrapper from "../components/SessionWrapper";
+import SessionInput from "../components/SessionInput";
 
-const EstablishmentAddress = () => (
+const EstablishmentAddress = props => (
   <FsaLayout>
-    <Header level={2}>What is the operators address?</Header>
-    <InputField hint={"First line of address"} />
-    <InputField hint={"Postcode"} />
+    <Header level={2}>Establishment address</Header>
+
+    <form action="/continue/establishment-address" method="post">
+      <SessionInput {...props} />
+      <Label>First line of address</Label>
+      <Input
+        name="establishment_first_line"
+        id="establishment_first_line"
+        // TODO APM: Decide on and implement validation for first line of address
+        //Work out why validator errors fails in test
+        error={props.validatorErrors["establishment_first_line"]}
+      />
+      <Label>Street</Label>
+      <Input
+        type="text"
+        id="establishment_street"
+        name="establishment_street"
+        error={props.validatorErrors["establishment_street"]}
+      />
+      <Label>Town or city</Label>
+      <Input
+        type="text"
+        id="establishment_town"
+        name="establishment_town"
+        error={props.validatorErrors["establishment_town"]}
+      />
+      <Label>Postcode</Label>
+      <Input
+        type="text"
+        id="establishment_postcode"
+        name="establishment_postcode"
+        error={props.validatorErrors["establishment_postcode"]}
+      />
+      <Button type="submit">Continue</Button>
+      <p>
+        If you are registering a mobile food business, please use the location
+        where it is normally stored overnight.
+      </p>
+    </form>
   </FsaLayout>
 );
 
-export default EstablishmentAddress;
+export default SessionWrapper(EstablishmentAddress);
