622e95ddfff01388e8e0df10ef536a868e36f110
A. P. Malde <anisha.malde@ibm.com>
2018-06-04 10:40:31 +0100

    Navigation

--- pages/application-complete.js
+++ pages/application-complete.js
@@ -1,11 +1,11 @@
-import React from "react";
+import SessionWrapper from "../components/SessionWrapper";
 import FsaLayout from "../components/FsaLayout";
 import { Header } from "govuk-react";
 
-const ApplicationComplete = () => (
+const ApplicationComplete = props => (
   <FsaLayout>
     <Header level={2}>Application Complete</Header>
   </FsaLayout>
 );
 
-export default ApplicationComplete;
+export default SessionWrapper(ApplicationComplete);
--- pages/declaration.js
+++ pages/declaration.js
@@ -1,32 +1,48 @@
-import React from "react";
 import FsaLayout from "../components/FsaLayout";
+import SessionWrapper from "../components/SessionWrapper";
+import SessionInput from "../components/SessionInput";
 import { Header, Checkbox, LeadParagraph, Button } from "govuk-react";
-const Declaration = () => (
-  <FsaLayout>
-    <form action="http://foo.com" method="post">
-      <Header level={2}>Declaration</Header>
 
+const Declaration = props => (
+  <FsaLayout>
+    <form action="/continue/declaration" method="post">
+      <SessionInput {...props} />
+      <Header level={2}>Declaration</Header>
       <LeadParagraph>
         Before submitting this food business registration, please review these
         statements. Tick each box to agree.
       </LeadParagraph>
-      <Checkbox name="checkBox" value="1">
+      <Checkbox
+        name="declaration1"
+        value="I declare that the information I have given on this form is correct and
+        complete to the best of my knowledge and belief."
+      >
         I declare that the information I have given on this form is correct and
         complete to the best of my knowledge and belief.
       </Checkbox>
-      <Checkbox name="checkBox" value="2">
+      <Checkbox
+        name="declaration2"
+        value="I, or the operator, will notify food authorities of any significant
+        changes to the business activity, including closure, within 28 days of
+        the change happening."
+      >
         I, or the operator, will notify food authorities of any significant
         changes to the business activity, including closure, within 28 days of
         the change happening.
       </Checkbox>
-      <Checkbox name="checkBox" value="3">
+      <Checkbox
+        name="declaration3"
+        value="I, or the operator, understands the operator is legally responsible for
+        the safety and authenticity of the food being produced or served at this
+        establishment."
+      >
         I, or the operator, understands the operator is legally responsible for
         the safety and authenticity of the food being produced or served at this
         establishment.
       </Checkbox>
-      <Button type="submit"> Submit</Button>
+      <Button type="submit">Submit</Button>
     </form>
   </FsaLayout>
 );
 
-export default Declaration;
+export default SessionWrapper(Declaration);
