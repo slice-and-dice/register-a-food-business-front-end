66bf0ff1676edb28264f34490f7a8450e74027d9
Django Shelton <djangoshelton@gmail.com>
2018-06-13 10:50:07 +0100

    validation functions and tests

---
+++ services/validationFunctions.js
@@ -0,0 +1,36 @@
+const { isPostalCode, isAscii, isEmpty, trim } = require("validator");
+
+const validateDeclaration = declaration => {
+  return declaration ? true : false;
+};
+
+const validatePostCode = postcode => {
+  if (typeof postcode === "string") {
+    return isPostalCode(postcode, "GB") ? true : false;
+  }
+  return false;
+};
+
+const validateEstablishmentFirstLine = firstLine => {
+  if (typeof firstLine === "string") {
+    if (isEmpty(trim(firstLine))) return false;
+    return isAscii(firstLine) ? true : false;
+  }
+  return false;
+};
+
+const validateStreet = street => {
+  if (typeof street === "string") {
+    if (isEmpty(street)) return true;
+    if (isEmpty(trim(street))) return false;
+    return isAscii(street) ? true : false;
+  }
+  return false;
+};
+
+module.exports = {
+  validateDeclaration,
+  validatePostCode,
+  validateEstablishmentFirstLine,
+  validateStreet
+};
