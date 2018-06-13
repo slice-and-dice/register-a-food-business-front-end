26746d239f8573703151b8fbd280bbbc1448f562
Joe Brady <joe.brady@uk.ibm.com>
2018-06-13 17:16:17 +0100

    SDB-206 Make continue button navigate and persist data

--- pages/registration-summary.js
+++ pages/registration-summary.js
@@ -1,10 +1,13 @@
 import FsaLayout from "../components/FsaLayout";
 import SessionWrapper from "../components/SessionWrapper";
-import { Header } from "govuk-react";
+import { Header, Button } from "govuk-react";
 
 const RegistrationSummary = props => (
   <FsaLayout>
     <Header level={3}>Registration summary</Header>
+    <form action="/continue/registration-summary" method="post">
+      <Button type="submit">Continue</Button>
+    </form>
   </FsaLayout>
 );
 
