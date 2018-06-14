f75a45f91394c8a0af49e9f5eedc435e14b7c865
Joe Brady <joe.brady@uk.ibm.com>
2018-06-14 13:17:42 +0100

    Add registration-summary.test.js

--- __tests__/registration-summary.test.js
+++ __tests__/registration-summary.test.js
@@ -1,5 +1,5 @@
 import RegistrationSummary from "../pages/registration-summary";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import renderer from "react-test-renderer";
 import * as emotion from "emotion";
 import { createSerializer } from "jest-emotion";
@@ -15,5 +15,25 @@ describe("<RegistrationSummary />", () => {
   it("matches the previous snapshot", () => {
     const tree = renderer.create(<RegistrationSummary />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("gets given props", () => {
+    const cumulativeAnswers = {
+      establishment_first_line: "Example first line"
+    };
+    const wrapper = mount(
+      <RegistrationSummary cumulativeAnswers={cumulativeAnswers} />
+    );
+    const establishmentFirstLine = wrapper.props().cumulativeAnswers
+      .establishment_first_line;
+    expect(establishmentFirstLine).toBe("Example first line");
+  });
+
+  describe("SummaryTable component", () => {
+    it("renders", () => {
+      const wrapper = mount(<RegistrationSummary cumulativeAnswers />);
+      const summaryTable = wrapper.find("SummaryTable");
+      expect(summaryTable.length).toBe(1);
+    });
   });
 });
--- pages/registration-summary.js
+++ pages/registration-summary.js
@@ -1,10 +1,16 @@
 import FsaLayout from "../components/FsaLayout";
+import SummaryTable from "../components/SummaryTable";
 import SessionWrapper from "../components/SessionWrapper";
 import { Header, Button } from "govuk-react";
 
 const RegistrationSummary = props => (
   <FsaLayout>
-    <Header level={3}>Registration summary</Header>
+    <Header level={3}>
+      Please check that the details you entered are correct.
+    </Header>
+
+    <SummaryTable {...props.cumulativeAnswers} />
+
     <form action="/continue/registration-summary" method="post">
       <Button type="submit">Continue</Button>
     </form>
