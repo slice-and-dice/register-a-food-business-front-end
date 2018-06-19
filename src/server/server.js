6ddacdfc34243538fbcf424fe867cff451b21911
Django Shelton <djangoshelton@gmail.com>
2018-06-19 13:23:52 +0100

    server.js tests

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const winston = require("winston");
 const routes = require("./routes");
-const Next = require("./next");
+const { Next } = require("./next");
 
-const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
-
-const startServer = async () => {
+module.exports = async () => {
   const app = express();
-
   ////////////////
   // WARNING: the below uses in-memory session storage and must be replaced.
   app.use(
@@ -27,14 +22,7 @@ const startServer = async () => {
 
   await Next.prepare();
 
-  app.use(routes);
+  app.use(routes());
 
-  app.listen(port);
-  winston.info(
-    `App running in ${
-      dev ? "DEVELOPMENT" : "PRODUCTION"
-    } mode on http://localhost:${port}`
-  );
+  return app;
 };
-
-startServer();
