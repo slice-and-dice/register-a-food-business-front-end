9ac8c30056788788c71bd4ffb89809fa1f861dec
Django Shelton <djangoshelton@gmail.com>
2018-06-26 11:58:07 +0100

    add logging and skip install step

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { handle } = require("./next");
+const { info } = require("winston");
 
 const continueController = require("./controllers/continue.controller");
 const submitController = require("./controllers/submit.controller");
@@ -8,6 +9,7 @@ module.exports = () => {
   const router = Router();
 
   router.post("/continue/:originator", (req, res) => {
+    info("continue route called");
     const response = continueController(
       `/${req.params.originator}`,
       req.session.cumulativeAnswers,
@@ -16,7 +18,7 @@ module.exports = () => {
 
     req.session.cumulativeAnswers = response.cumulativeAnswers;
     req.session.validatorErrors = response.validatorErrors;
-
+    info(`continue route finished with route ${response.redirectRotue}`);
     res.redirect(response.redirectRoute);
   });
 
@@ -25,12 +27,15 @@ module.exports = () => {
   });
 
   router.get("/submit", async (req, res) => {
+    info("submit route called");
     const response = await submitController(req.session.cumulativeAnswers);
-
+    info(`submit route finished with route ${response.redirectRotue}`);
     res.redirect(response.redirectRoute);
   });
 
   router.get("*", (req, res) => {
+    info("* route called");
+    info(`* route finished`);
     handle(req, res);
   });
 
