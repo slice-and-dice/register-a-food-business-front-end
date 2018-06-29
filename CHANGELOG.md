15c4c47afdb5441ad846c781c97ecbaa02ff45ed
Joe Brady <joe.brady@uk.ibm.com>
2018-06-29 17:07:38 +0100

    Add charity details page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## v1.14.0 - 2nd July 2018 (unreleased)
+
+### Added
+
+* SDB-40 Charity details
+
 ## v1.13.3 - 29th June 2018 (unreleased)
 
 ### Modified
---
+++ pages/operator-charity-details.js
@@ -0,0 +1,64 @@
+import {
+  FsaLayout,
+  SessionWrapper,
+  ContentItem,
+  BackButton,
+  ContinueButton
+} from "../src/components";
+import { Header, InputField, HiddenText, Paragraph } from "govuk-react";
+
+const OperatorCharityDetails = props => (
+  <FsaLayout>
+    <BackButton originator="operator-charity-details" />
+    <Header level={2}>Details of the operating charity</Header>
+
+    <HiddenText summaryText={"What is a food business operator?"}>
+      <Paragraph>
+        A food business operator is the person, charity or company who makes the
+        decisions about the food business, what it serves and how it operates.
+      </Paragraph>
+    </HiddenText>
+
+    <form action="/continue/operator-charity-details" method="post">
+      <ContentItem.B_30_15>
+        <ContentItem.B_30_15>
+          <InputField
+            input={{
+              name: "operator_charity_name",
+              defaultValue: props.cumulativeAnswers.operator_charity_name,
+              autoComplete: "off"
+            }}
+            id="operator_charity_name"
+            meta={{
+              touched: true,
+              error: props.validatorErrors["operator_charity_name"]
+            }}
+          >
+            Charity name
+          </InputField>
+        </ContentItem.B_30_15>
+
+        <ContentItem.B_30_15>
+          <InputField
+            input={{
+              name: "operator_charity_number",
+              defaultValue: props.cumulativeAnswers.operator_charity_number,
+              autoComplete: "off"
+            }}
+            id="operator_charity_number"
+            meta={{
+              touched: true,
+              error: props.validatorErrors["operator_charity_number"]
+            }}
+          >
+            Charity reference number
+          </InputField>
+        </ContentItem.B_30_15>
+      </ContentItem.B_30_15>
+
+      <ContinueButton />
+    </form>
+  </FsaLayout>
+);
+
+export default SessionWrapper(OperatorCharityDetails);
