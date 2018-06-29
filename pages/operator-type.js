c45012154a613c5f9b79b209e0d39ccfa03a6010
Joe Brady <joe.brady@uk.ibm.com>
2018-06-29 12:54:04 +0100

    Remove ContentItem wrappers where too much spacing

--- pages/operator-type.js
+++ pages/operator-type.js
@@ -23,57 +23,55 @@ const OperatorType = props => (
 
     <form action="/continue/operator-type" method="post">
       <ContentItem.B_30_15>
-        <ContentItem.B_30_15>
-          <MultiChoice
-            meta={{
-              touched: true,
-              error: props.validatorErrors.operator_type
-            }}
+        <MultiChoice
+          meta={{
+            touched: true,
+            error: props.validatorErrors.operator_type
+          }}
+        >
+          <Radio
+            name="operator_type"
+            value="A person"
+            id="operator_type_person"
+            defaultChecked={
+              props.cumulativeAnswers.operator_type === "A person"
+            }
           >
-            <Radio
-              name="operator_type"
-              value="A person"
-              id="operator_type_person"
-              defaultChecked={
-                props.cumulativeAnswers.operator_type === "A person"
-              }
-            >
-              A person
-              <HintTextSmall>
-                The food business is owned or operated by an individual person,
-                rather than any legal entity such as a business or charity.
-              </HintTextSmall>
-            </Radio>
-            <Radio
-              name="operator_type"
-              value="A company"
-              id="operator_type_company"
-              defaultChecked={
-                props.cumulativeAnswers.operator_type === "A company"
-              }
-            >
-              A company
-              <HintTextSmall>
-                The food business is owned or operated by a limited company
-                rather than a single person or partnership of people.
-              </HintTextSmall>
-            </Radio>
-            <Radio
-              name="operator_type"
-              value="A charity"
-              id="operator_type_charity"
-              defaultChecked={
-                props.cumulativeAnswers.operator_type === "A charity"
-              }
-            >
-              A charity
-              <HintTextSmall>
-                The food business is owned or operated by a charity rather than
-                a company, person or partnership of people.
-              </HintTextSmall>
-            </Radio>
-          </MultiChoice>
-        </ContentItem.B_30_15>
+            A person
+            <HintTextSmall>
+              The food business is owned or operated by an individual person,
+              rather than any legal entity such as a business or charity.
+            </HintTextSmall>
+          </Radio>
+          <Radio
+            name="operator_type"
+            value="A company"
+            id="operator_type_company"
+            defaultChecked={
+              props.cumulativeAnswers.operator_type === "A company"
+            }
+          >
+            A company
+            <HintTextSmall>
+              The food business is owned or operated by a limited company rather
+              than a single person or partnership of people.
+            </HintTextSmall>
+          </Radio>
+          <Radio
+            name="operator_type"
+            value="A charity"
+            id="operator_type_charity"
+            defaultChecked={
+              props.cumulativeAnswers.operator_type === "A charity"
+            }
+          >
+            A charity
+            <HintTextSmall>
+              The food business is owned or operated by a charity rather than a
+              company, person or partnership of people.
+            </HintTextSmall>
+          </Radio>
+        </MultiChoice>
       </ContentItem.B_30_15>
 
       <ContinueButton />
--- pages/registration-role.js
+++ pages/registration-role.js
@@ -14,45 +14,43 @@ const RegistrationRole = props => (
 
     <form action="/continue/registration-role" method="post">
       <ContentItem.B_30_15>
-        <ContentItem.B_30_15>
-          <MultiChoice
-            meta={{
-              touched: true,
-              error: props.validatorErrors.registration_role
-            }}
+        <MultiChoice
+          meta={{
+            touched: true,
+            error: props.validatorErrors.registration_role
+          }}
+        >
+          <Radio
+            name="registration_role"
+            value="Sole trader"
+            id="registration_role_sole_trader"
+            defaultChecked={
+              props.cumulativeAnswers.registration_role === "Sole trader"
+            }
           >
-            <Radio
-              name="registration_role"
-              value="Sole trader"
-              id="registration_role_sole_trader"
-              defaultChecked={
-                props.cumulativeAnswers.registration_role === "Sole trader"
-              }
-            >
-              I operate it as a sole trader
-            </Radio>
-            <Radio
-              name="registration_role"
-              value="Partnership"
-              id="registration_role_partnership"
-              defaultChecked={
-                props.cumulativeAnswers.registration_role === "Partnership"
-              }
-            >
-              I operate it in a partnership
-            </Radio>
-            <Radio
-              name="registration_role"
-              value="Representative"
-              id="registration_role_representative"
-              defaultChecked={
-                props.cumulativeAnswers.registration_role === "Representative"
-              }
-            >
-              I represent a person, charity or company which operates it
-            </Radio>
-          </MultiChoice>
-        </ContentItem.B_30_15>
+            I operate it as a sole trader
+          </Radio>
+          <Radio
+            name="registration_role"
+            value="Partnership"
+            id="registration_role_partnership"
+            defaultChecked={
+              props.cumulativeAnswers.registration_role === "Partnership"
+            }
+          >
+            I operate it in a partnership
+          </Radio>
+          <Radio
+            name="registration_role"
+            value="Representative"
+            id="registration_role_representative"
+            defaultChecked={
+              props.cumulativeAnswers.registration_role === "Representative"
+            }
+          >
+            I represent a person, charity or company which operates it
+          </Radio>
+        </MultiChoice>
       </ContentItem.B_30_15>
 
       <ContinueButton />
