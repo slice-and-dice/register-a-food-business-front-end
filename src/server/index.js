27aa4bfea3f905a8568d8f050c087df867f30b20
Django Shelton <djangoshelton@gmail.com>
2018-07-10 16:35:39 +0100

    add mongo url to config and import to index

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,11 @@
 const { info } = require("winston");
 const server = require("./server");
-
+const { MONGODB_URL } = require("./config");
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
 
 const startServer = async () => {
-  const app = await server();
+  const app = await server(MONGODB_URL);
   app.listen(port, () =>
     info(
       `App running in ${
