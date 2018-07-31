81fa354cf05c03ac33d5a708d488847f0ee61efa
A. P. Malde <anisha.malde@ibm.com>
2018-07-31 10:01:57 +0100

    update to test

--- src/__tests__/summary-confirmation.test.js
+++ src/__tests__/summary-confirmation.test.js
@@ -1,10 +1,15 @@
 import ApplicationComplete from "../../pages/summary-confirmation";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import renderer from "react-test-renderer";
 import * as emotion from "emotion";
 import { createSerializer } from "jest-emotion";
-
+import { transformAnswersForSummary } from "../server/services/data-transform.service";
+jest.mock("../server/services/data-transform.service");
 expect.addSnapshotSerializer(createSerializer(emotion));
+
+const cumulativeAnswers = {
+  establishment_first_line: "Example first line"
+};
 
 describe("<ApplicationComplete />", () => {
   it("renders without crashing", () => {
@@ -15,5 +20,29 @@ describe("<ApplicationComplete />", () => {
   it("matches the previous snapshot", () => {
     const tree = renderer.create(<ApplicationComplete />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe("SummaryTable component", () => {
+    it("Gets given transformedAnswers ", () => {
+      transformAnswersForSummary.mockImplementation(() => ({ test: "answer" }));
+      const wrapper = mount(
+        <ApplicationComplete
+          cumulativeAnswers={cumulativeAnswers}
+          hideChangeButtons={true}
+        />
+      );
+      const summaryTable = wrapper.find("SummaryTable");
+      expect(summaryTable.props().test).toBe("answer");
+    });
+    it("renders", () => {
+      const wrapper = mount(
+        <ApplicationComplete
+          cumulativeAnswers={cumulativeAnswers}
+          hideChangeButtons={true}
+        />
+      );
+      const summaryTable = wrapper.find("SummaryTable");
+      expect(summaryTable.length).toBe(1);
+    });
   });
 });
