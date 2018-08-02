edd2404522f9db3d90596353e6550fbddacfc921
Joe Brady <joe.brady@uk.ibm.com>
2018-08-02 09:35:37 +0100

    IE11 bugfix: add Object.keys polyfill

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ module.exports = withCSS({
         use: "eslint-loader"
       });
     }
+
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("./client/polyfills.js")
+      ) {
+        entries["main.js"].unshift("./client/polyfills.js");
+      }
+
+      return entries;
+    };
     return config;
   }
 });
