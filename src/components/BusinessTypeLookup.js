2092c3f45e6fd2885ce757daabddb7319ce2cbad
Joe Brady <joe.brady@uk.ibm.com>
2018-08-03 15:11:17 +0100

    Add searchTerm and displayName combined search

--- src/components/BusinessTypeLookup.js
+++ src/components/BusinessTypeLookup.js
@@ -9,12 +9,8 @@ const suggest = (query, returnResultsArray) => {
     JSON.parse(JSON.stringify(businessTypesJSON))
   );
 
-  let displayNameMatchArray;
-  let searchTermMatchArray;
-  let resultsArray;
-
-  const checkForMatch = (displayNameOrSearchTerm, query) => {
-    const displayNameOrSearchTermArray = displayNameOrSearchTerm
+  const checkForDisplayNameMatch = (displayName, query) => {
+    const displayNameWordArray = displayName
       .toLowerCase()
       .split(" ")
       .filter(word => word !== "");
@@ -28,29 +24,86 @@ const suggest = (query, returnResultsArray) => {
     queryArray.forEach(word => {
       if (matching === true) {
         matching =
-          displayNameOrSearchTermArray.findIndex(entry =>
-            entry.startsWith(word)
-          ) !== -1;
+          displayNameWordArray.findIndex(entry => entry.startsWith(word)) !==
+          -1;
       }
     });
     return matching;
   };
 
+  const checkForSearchTermMatch = (searchTerm, displayName, query) => {
+    const searchTermStringArray = searchTerm
+      .toLowerCase()
+      .split(" ")
+      .filter(word => word !== "");
+
+    const displayNameStringArray = displayName
+      .toLowerCase()
+      .split(" ")
+      .filter(word => word !== "");
+
+    const queryArray = query
+      .toLowerCase()
+      .split(" ")
+      .filter(word => word !== "");
+
+    let queryWordsNotInSearchTerm = [];
+
+    queryArray.forEach(word => {
+      if (
+        searchTermStringArray.findIndex(entry => entry.startsWith(word)) === -1
+      ) {
+        queryWordsNotInSearchTerm.push(word);
+      }
+    });
+
+    // if none of the query words matched the search term, it's not a match
+    if (queryArray.length === queryWordsNotInSearchTerm.length) {
+      return false;
+      // if all of the query words matched the search term, it's a match
+    } else if (queryWordsNotInSearchTerm.length === 0) {
+      return true;
+      // if the search term is matched and there is at least one remaining word unmatched,
+      // then search the display name as well
+    } else {
+      let remainingQueryWordsMatchDisplayName = true;
+
+      queryWordsNotInSearchTerm.forEach(word => {
+        if (remainingQueryWordsMatchDisplayName === true) {
+          remainingQueryWordsMatchDisplayName =
+            displayNameStringArray.findIndex(entry =>
+              entry.startsWith(word)
+            ) !== -1;
+        }
+      });
+
+      return remainingQueryWordsMatchDisplayName;
+    }
+  };
+
+  let displayNameMatchArray;
+  let searchTermMatchArray;
+  let resultsArray;
+
   if (query) {
     displayNameMatchArray = businessTypesArray
+      // remove duplicate entries of the same displayName
       .filter(
         (obj, pos, arr) =>
           arr
             .map(mapObj => mapObj["displayName"])
             .indexOf(obj["displayName"]) === pos
       )
-      .filter(entry => checkForMatch(entry.displayName, query))
+      // check for matching words and beginnings of words
+      .filter(entry => checkForDisplayNameMatch(entry.displayName, query))
+      // remove the searchTerm field of each result
       .map(entry => {
         return { displayName: entry.displayName, searchTerm: undefined };
       });
 
     searchTermMatchArray = businessTypesArray.filter(entry =>
-      checkForMatch(entry.searchTerm, query)
+      // check for matching words and beginnings of words (including the displayName)
+      checkForSearchTermMatch(entry.searchTerm, entry.displayName, query)
     );
 
     resultsArray = displayNameMatchArray.concat(searchTermMatchArray);
@@ -82,7 +135,7 @@ const templates = {
 
 const BusinessTypeLookup = props => (
   <div>
-    <Autocomplete source={suggest} templates={templates} />
+    <Autocomplete source={suggest} templates={templates} autoselect={true} />
   </div>
 );
 
